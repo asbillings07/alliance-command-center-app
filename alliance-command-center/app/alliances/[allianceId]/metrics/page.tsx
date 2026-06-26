1758cabae81b3fe6c0e109d033388ebbbd3f56a6
Aaron Billings <aaron.billings@wbd.com>
2026-06-26 15:46:45 -0400

    update css

--- alliance-command-center/app/alliances/[allianceId]/metrics/page.tsx
+++ alliance-command-center/app/alliances/[allianceId]/metrics/page.tsx
@@ -19,15 +19,11 @@ export default async function MetricsPage({ params }: Params) {
         },
     });
 
-    if (metrics.length === 0) {
-        return <div className="text-2xl font-bold">No metrics found, create one to get started!</div>;
-    }
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4">
       <h1 className="text-2xl font-bold">Metrics</h1>
-      <div>
-        <button className="w-full mb-4 rounded-md border-2 border-dashed border-gray-300 p-4 text-gray-500 hover:border-blue-400 hover:text-blue-500 cursor-pointer">Create Metric</button>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <button className="w-full rounded-md border-2 border-dashed border-gray-300 p-4 text-gray-500 hover:border-blue-400 hover:text-blue-500 cursor-pointer">Create Metric</button>
         {metrics.length > 0 ? (
           metrics.map((metric) => (
             <MetricCard key={metric.id} metric={metric} />
